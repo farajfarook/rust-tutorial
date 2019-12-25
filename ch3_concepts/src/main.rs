adba02c10b603b52e77e32f629eea6e264c22775
Faraj Farook <farajfarook@gmail.com>
2019-12-25 21:32:45 +0530

    adding concepts examples

---
+++ ch3_concepts/src/main.rs
@@ -0,0 +1,124 @@
+const MAX_POINTS: u32 = 100_000;
+
+fn main() {
+    // Mutability
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    // Constants
+    println!("Constant is {}", MAX_POINTS);
+
+    // Shadowing
+    let y = 10;
+    println!("The value of y is: {}", y);
+    let y = 11;
+    println!("The value of y is: {}", y);
+
+    // Visual seperators
+    let z: i32 = 98_000;
+    println!("Value Z {}", z);
+    let z: i32 = 0x77;
+    println!("Value Z {}", z);
+    let z: i32 = 0o55;
+    println!("Value Z {}", z);
+    let z: i32 = 0b1111_0000;
+    println!("Value Z {}", z);
+    let z: u8 = b'B';
+    println!("Value Z {}", z);
+
+    // Chars
+    let c = 'z';
+    let z = 'ℤ';
+    let heart_eyed_cat = '😻';
+    println!("{} {} {}", c, z, heart_eyed_cat);
+
+    // Tuples
+    let tup = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+    println!("The value of z is: {}", z);
+
+    println!("The value of tup.0 is: {}", tup.0);
+    println!("The value of tup.1 is: {}", tup.1);
+    println!("The value of tup.2 is: {}", tup.2);
+
+    // Arrays
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    println!("Months: {}", months[2]);
+
+    let ret = another_function(5, 6);
+    println!("Returns {}", ret);
+
+    // IF conditions
+    let number = 6;
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {}", number);
+
+    // While loop
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
+    // For loop
+    let a = [10, 20, 30, 40, 50];
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
+
+    // Loop var assign
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
+}
+
+fn another_function(x: i32, y: i32) -> i32 {
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+    x + y
+}
