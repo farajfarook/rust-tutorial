d64dee8cb66f4584225c714483ac18e750b2dba8
Faraj Farook <farajfarook@gmail.com>
2022-01-07 20:47:39 +1100

    managing projects

--- manage_proj/src/lib.rs
+++ manage_proj/src/lib.rs
@@ -1,22 +1,49 @@
-mod front_of_house {
-    mod hosting {
-        fn add_to_wait_list() {
-            // ...
+mod front_of_house;
+
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_wait_list();
+
+    // Relative path
+    front_of_house::hosting::add_to_wait_list();
+
+    // use
+    hosting::add_to_wait_list();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    println!("I'd like {} toast please", meal.toast);
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+}
+
+fn serve_order() {}
+
+mod back_of_house {
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
         }
-        fn seat_at_table() {
-            // ...
-        }
+    }
+    pub enum Appetizer {
+        Soup,
+        Salad,
     }
 
-    mod serving {
-        fn take_order() {
-            // ...
-        }
-        fn serve_order() {
-            // ...
-        }
-        fn take_payment() {
-            // ...
-        }
+    fn fix_incorrect_order() {
+        cook_order();
+        super::serve_order();
     }
+
+    fn cook_order() {}
 }
--- manage_proj/src/main.rs
+++ manage_proj/src/main.rs
@@ -1,3 +1,16 @@
+use std::collections::HashMap;
+use std::fmt::Result;
+use std::io::Result as IoResult;
+
 fn main() {
-    println!("Hello, world!");
+    let mut map = HashMap::new();
+    map.insert(String::from("key"), String::from("value"));
+}
+
+fn function1() -> Option<Result> {
+    None
+}
+
+fn function2() -> Option<IoResult<u8>> {
+    None
 }
