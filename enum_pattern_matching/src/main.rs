c5fdc5cfdb14e9f5bf2f1b4c3682ab73898d13b6
Faraj Farook <farajfarook@gmail.com>
2022-01-07 20:10:31 +1100

    match control

--- enum_pattern_matching/src/main.rs
+++ enum_pattern_matching/src/main.rs
@@ -15,6 +15,85 @@ fn main() {
     message.call();
     let message = Message::ChangeColor(0, 255, 255);
     message.call();
+
+    // Option
+    let some_number = Some(5);
+    let some_string = Some("a string");
+    let absent_number: Option<i32> = None;
+
+    println!("{:?}", some_number);
+    println!("{:?}", some_string);
+
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+    
+    let num = x + y.unwrap_or_else(|| 0);
+    println!("{}", num);    
+
+    // Match
+    let coin = Coin::Quarter(UsState::Alabama);
+    println!("{}", coin.value_in_cents());
+
+    // Match with options
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    println!("{:?}, {:?}", six, none);
+
+    // Catch all
+    let dice_roll = 9;
+    match dice_roll {
+        7 => println!("You win!"),
+        _ => println!("You lose!"),
+    }
+
+    // If let
+    let config_max = Some(3u8);
+    match config_max {
+        Some(max) => println!("Max is {}", max),
+        _ => (),
+    }
+
+    if let Some(max) = config_max {
+        println!("Max is {}", max);
+    }
+}
+
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // ...
+}
+
+#[derive(Debug)]
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+impl Coin {
+    fn value_in_cents(&self) -> u32 {
+        match self {
+            Coin::Penny => 1,
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter(state) => {
+                println!("State quarter from {:?}!", state);
+                25
+            },
+        }
+    }
 }
 
 #[derive(Debug)]
