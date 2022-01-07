997c2cc7ac4f6fcb8d4d844098645e339d3659ea
Faraj Farook <farajfarook@gmail.com>
2022-01-07 16:00:57 +1100

    Slices

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -35,6 +35,34 @@ fn main() {
     let mut s = String::from("hello");
     change(&mut s);
     println!("s = {}", s);
+
+    // String Slice
+    let s = String::from("hello world");
+    let word = first_word(&s[..]);
+    println!("{}", word);
+    let word = first_word(&s);
+    println!("{}", word);
+
+    let s_ltr = "hello world";
+    let word = first_word(&s_ltr[..]);
+    println!("{}", word);
+    let word = first_word(s_ltr);
+    println!("{}", word);
+
+    // Other slices
+    let a = [1, 2, 3, 4, 5];
+    let slice = &a[1..3];
+    assert_eq!(slice, &[2, 3]);
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {            
+            return &s[0..i];
+        }    
+    }
+    &s[..]
 }
 
 fn change(s: &mut String) {
