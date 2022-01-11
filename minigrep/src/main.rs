0ec0e1beae7ecde342b0ae3edda2024e7a8d3b12
Faraj Farook <farajfarook@gmail.com>
2022-01-11 15:37:22 +1100

    Adding grep

---
+++ minigrep/src/main.rs
@@ -0,0 +1,20 @@
+mod lib;
+use lib::*;
+use std::env;
+use std::process;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {} in {}", config.query, config.filename);
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
